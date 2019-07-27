31fe2985cd24bdb8dc1355f242cb0cf5fef4a3e9
unknown <ianmnicholas@gmail.com>
2019-07-26 22:36:31 -0400

    Flask

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-def handle_request(environ):
-	return (
-		'text/plain',
-		'200 OK',
-		'app works',
-	)
+from flask import Flask, render_template
+App = Flask(__name__)
+
+@App.route('/')
+def index():
+	return 'index'
+
+if __name__ == '__main__':
+	App.run()
--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,13 +1,3 @@
-import os
 import sys
-
-from app import handle_request
-
-
+from app import MyApp as application
 sys.path.insert(0, os.path.dirname(__file__))
-
-
-def application(environ, start_response):
-	content_type, response_code, body = handle_request(environ)
-	start_response(response_code, [('Content-Type', content_type)])
-	return [body.encode()]
