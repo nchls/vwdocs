ecf0798dfb713b49510fd6f784feaec4b366841f
unknown <ianmnicholas@gmail.com>
2019-07-26 22:01:30 -0400

    WSGI and app Python scripts

---
+++ app.py
@@ -0,0 +1,6 @@
+def handle_request(environ):
+	return (
+		'text/plain',
+		'200 OK',
+		'app works',
+	)
---
+++ passenger_wsgi.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+from app import handle_request
+
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+
+def application(environ, start_response):
+	content_type, response_code, body = handle_request(environ)
+	start_response(response_code, [('Content-Type', content_type)])
+	return [response.encode()]
+
+import os_wsgi.py
